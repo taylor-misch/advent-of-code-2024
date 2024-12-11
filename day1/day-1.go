d5e9c4fe5e4677a4c789880ac7a0635d1f4c68fb
Taylor Misch <taylor.misch@datasite.com>
2024-12-11 15:20:12 -0600

    Set up project structure

---
+++ day1/day-1.go
@@ -0,0 +1,23 @@
+package day1
+
+import (
+	"advent-of-code-2024/utilities"
+	"fmt"
+)
+
+func Part1() {
+	fmt.Println("Part 1 Begin")
+	i := utilities.ReadInput("day1/day-1-input.txt")
+	fmt.Println(i)
+
+	fmt.Println("Part 1 End")
+}
+
+func Part2() {
+	fmt.Println("Part 2 Begin")
+	i := utilities.ReadInput("day1/day-1-input.txt")
+	fmt.Println(i)
+
+	fmt.Println("Part 2 End")
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module advent-of-code-2024
 
-go 1.22
+go 1.23
